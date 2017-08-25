aae754e300ebd92d29159346c80edbfe931b0003
Lechu67 <l.szajnowski@gmail.com>
2017-08-25 22:54:55 +0200

    Spring security enable

--- src/main/java/com/springboot/web/springbootfirstwebapp/controller/TodoController.java
+++ src/main/java/com/springboot/web/springbootfirstwebapp/controller/TodoController.java
@@ -7,6 +7,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -15,14 +17,11 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
 import com.springboot.web.springbootfirstwebapp.model.Todo;
 import com.springboot.web.springbootfirstwebapp.service.TodoService;
 
 
 @Controller
-@SessionAttributes("name")
 public class TodoController {
 	
 	@Autowired
@@ -43,7 +42,12 @@ public class TodoController {
 	}
 
 	private String getLoggedInUserName(ModelMap model) {
-		return (String) model.get("name");
+		Object principal = 
+				SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if(principal instanceof UserDetails )
+			return ((UserDetails) principal).getUsername();
+	
+	return principal.toString();
 	}
 	@RequestMapping(value="/todo", method=RequestMethod.GET)
 	public String showFormForAdding( ModelMap model){
